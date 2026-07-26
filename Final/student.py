bc8a7f563cd49eedecda513e11823e6968f398d1
Tri Viet <viettringuyen207@gmail.com>
2026-07-26 15:18:18 +0700

    Delete Final directory

--- Final/student.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from flask import Blueprint, render_template, request, redirect
-
-student_bp = Blueprint("student", __name__)
-
-@student_bp.route("/students")
-def home():
-    return render_template("student.html")
